99156c7a3c45568d442a892163d8df59288a660b
Britton Forsyth <britton.forsyth@kcl.ac.uk>
2018-02-01 22:17:42 +0000

    Add initial graph URL link

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, url
 
 urlpatterns = [
-    path('graphs/', include('graphs.urls')),
-    path('admin/', admin.site.urls),
+    url(r'^graphs/', include('graphs.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
