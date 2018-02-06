9d5659c659a4d3d066b3d65de4c3d5dd33646441
Saadman Sakib <saadman.sakib@kcl.ac.uk>
2018-02-06 16:54:10 +0000

    Add forms.py views.py and commands.py

--- mysite/graphs/views.py
+++ mysite/graphs/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import UploadFileForm
+from ./management/commands import UploadedFiles
 
 
 def home(request) :
 	return render(request, 'index.html')
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
