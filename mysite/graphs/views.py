471eb185215884cfb8c7d7e60128cc9dbcf94c14
BadButNotBoujee <shayneah@googlemail.com>
2018-02-06 15:14:05 +0000

    Finishing off the initial framework for the user interface index

--- mysite/graphs/views.py
+++ mysite/graphs/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def index(request) :
-    return HttpResponse("Hello, this will contain graphs")
 
-# Create your views here.
+def home(request) :
+	return render(request, 'index.html')
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+#from django.conf.urls import include
+#from django.conf.urls import path
+from django.conf.urls import url
+
+from graphs import views
 
 urlpatterns = [
-    path('graphs/', include('graphs.urls')),
-    path('admin/', admin.site.urls),
+   # path('graphs/', include('graphs.urls')),
+   # path('admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
 ]
