a350187210e263b3ffbdd982ebb4ae47a8b23dfe
BadButNotBoujee <shayneah@googlemail.com>
2018-02-08 13:32:54 +0000

    #11 Implementing customised form model and form field, allowing programmers to dynamically add an arbitrary number of fields to be rendered

--- mysite/graphs/forms.py
+++ mysite/graphs/forms.py
@@ -1,2 +1,44 @@
 from django import forms
 
+class AbstractField(forms.MultipleChoiceField):
+	
+	__choicesInField = [("DEFAULT", "Select a value")]
+
+	def __init__(self, name, choice = [("DEFAULT", "Select a value")]):
+		self._name = name
+		super().__init__(required=False, widget=forms.Select, choices=choice)
+		
+	def addRecord(self, recordName):
+		self.__choicesInField.append(recordName)
+	
+	def name(self, aName = None):
+		if aName: self._name = aName
+		return self._name
+
+
+
+class FieldSelectionForm(forms.Form):
+	listOfFields = []
+	
+	def __init__(self, *args, **kwargs):
+		self.addFieldSelection(AbstractField(name ="DEFAULT"))
+		super(FieldSelectionForm, self).__init__(*args, **kwargs)
+
+	def addFieldSet(self, fields):
+		for field in fields:
+			self.addFieldSelection(field)
+			
+		self.populateForm()
+	
+	def addFieldSelection(self, field):
+		if isinstance(field, AbstractField):
+			field.label = field.name()
+			self.listOfFields.append(field)
+	
+	def populateForm(self):
+		for field in self.listOfFields:
+			self.fields['{name}'.format(name=field.name())] = field
+		
+		
+			
+			
--- mysite/graphs/views.py
+++ mysite/graphs/views.py
@@ -1,6 +1,26 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+from .forms import *
 
 def home(request) :
-	return render(request, 'index.html')
+	fields = []
+	
+	
+	for field in range(1, 20):
+		f = AbstractField("FIELD: {x}".format(x = field), [(("1", "RECORD 1"))])
+		fields.append(f)	
+	
+	
+	if request.method == 'POST':
+		form = FieldSelectionForm(request.POST)
+		form.addFieldSet(fields)
+		
+		if form.is_valid():
+			print("valid!") 
+	else:
+		form = FieldSelectionForm()
+		
+	return render(request, 'index.html', {'form': form})
+ 
+
