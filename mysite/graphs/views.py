54cabd827e4629cb0cc5f27148d4bc96a30089ee
Saadman Sakib <saadman.sakib@kcl.ac.uk>
2018-02-09 15:32:02 +0000

    Fix bug found regarding form not loading due to fluctuating request methods on varying browsers

--- mysite/graphs/views.py
+++ mysite/graphs/views.py
@@ -3,37 +3,46 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import *
 
+# BE CAREFUL OF REQUEST METHODS
 
 def home(request) :
-		
+
 	uploadForm = addUploadForm(request)
 	fieldsFrom = addFieldsForm(request)
-
+	print("HELLO")
 	return render(request, 'index.html', {'upload': uploadForm, 'fields': fieldsFrom})
- 
- 
- 
- 
+
+
+
+
 def addFieldsForm(request):
+	print("Fields form called")
 	fields = []
-	
+
 	for field in range(1, 20):
 		f = AbstractField("FIELD: {x}".format(x = field), [(("1", "RECORD 1"))])
-		fields.append(f)	
-	
+		fields.append(f)
+		print("Fields addition loop")
+
 	if request.method == 'POST':
 		form = FieldSelectionForm(request.POST)
 		form.addFieldSet(fields)
-		
+		print("Fields form created")
+
 		if form.is_valid():
-			print("valid!") 
+			print("valid!")
+		else:
+			print("not valid!")
+
 	else:
 		form = FieldSelectionForm()
-	
+		form.addFieldSet(fields)
+		print("blank Fields form created")
+
 	return form
 
 def addUploadForm(request):
-	
+
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
@@ -41,6 +50,5 @@ def addUploadForm(request):
             return HttpResponseRedirect('/success/url/')
     else:
         form = UploadFileForm()
-    
-    return form
 
+    return form
