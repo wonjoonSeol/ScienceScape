55b11ea7c48cd4ede43669c3fda00fee1e9908ae
Britton Forsyth <britton.forsyth@kcl.ac.uk>
2018-02-16 19:31:37 +0000

    Add in final authentication changes

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, url
 from django.contrib.auth import views as auth_views
+from registration.views import home, register
 
 urlpatterns = [
     url(r'^graphs/', include('graphs.urls')),
+    url(r'^/$', home),
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^register/$', register)
     
 ]
