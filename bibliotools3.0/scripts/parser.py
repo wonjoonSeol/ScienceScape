113059dd1a65d2793fc91e15b5fbc37588445e72
Nicolas Volken <32220848+nicolasvolken@users.noreply.github.com>
2018-02-19 18:39:35 +0000

    Remove useless import statement

--- bibliotools3.0/scripts/parser.py
+++ bibliotools3.0/scripts/parser.py
@@ -11,7 +11,7 @@ import os
 import sys
 import glob
 import argparse
-import Utils
+import utility
 
 common_words = ['a', 'able', 'about', 'across', 'after', 'all', 'almost', 'also', 'am', 'among', 'an', 'and', 'any', 'are', 'as', 'at', 'be', 'because', 'been', 'but', 'by', 'can', 'cannot', 'could', 'dear', 'did', 'do', 'does', 'either', 'else', 'ever', 'every', 'for', 'from', 'get', 'got', 'had', 'has', 'have', 'he', 'her', 'hers', 'him', 'his', 'how', 'however', 'i', 'if', 'in', 'into', 'is', 'it', 'its', 'just', 'least', 'let', 'like', 'likely', 'may', 'me', 'might', 'most', 'must', 'my', 'neither', 'no', 'nor', 'not', 'of', 'off', 'often', 'on', 'only', 'or', 'other', 'our', 'own', 'rather', 'said', 'say', 'says', 'she', 'should', 'since', 'so', 'some', 'than', 'that', 'the', 'their', 'them', 'then', 'there', 'these', 'they', 'this', 'tis', 'to', 'too', 'twas', 'us', 'wants', 'was', 'we', 'were', 'what', 'when', 'where', 'which', 'while', 'who', 'whom', 'why', 'will', 'with', 'would', 'yet', 'you', 'your']
 
@@ -46,7 +46,7 @@ def Wos_parser(in_dir, out_dir, verbose):
     srclst = glob.glob(srccomp)
     id = int(-1)
    
-    utility = Utility(srclst)
+    utility = utility.Utility(srclst)
 """
     dst1  = os.path.join(out_dir, "articles.dat")
     f_articles = open(dst1,'w')
