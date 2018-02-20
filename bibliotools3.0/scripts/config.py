fac1d3a6023727d3bf8d3a033f2f0793931ae41b
Paul Muresan <k1631503@kcl.ac.uk>
2018-02-20 23:08:19 +0000

    Add header & country codes to config.py (#16)

--- bibliotools3.0/scripts/config.py
+++ bibliotools3.0/scripts/config.py
@@ -93,5 +93,29 @@ CONFIG={
 	"punctuation": ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '.', '/',
 	        ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', ' - '],
 
-	#TODO Add dictionary for header codes
+	#HEADER CODES
+	'accession_number' : 'UT',
+	'authors' : 'AU',
+	'author_keywords' : 'DE',
+	'keywords_plus' : 'ID',
+	'document_title' : 'TI',
+	'wos_categories' : 'WC',
+	'cited_references' : 'CR',
+	'author_address' : 'C1',
+	'year_published' : 'PY',
+	'twenty_nine_character_source_abbreviation' : 'J9',
+	'volume' : 'VL',
+	'beginning_page' : 'BP',
+	'doi' : 'DI',
+	'publication_type' : 'PT',
+	'document_type' : 'DT',
+	'wos_core_collection_times_cited' : 'TC',
+
+	'usa_country_codes' : ['USA',
+			'AL ', 'AK ', 'AZ ', 'AR ', 'CA ', 'NC ', 
+			'SC ', 'CO ', 'CT ', 'ND ', 'SD ','DE ', 'FL ', 'GA ', 'HI ' , 'ID ', 
+			'IL ', 'IN ', 'IA ', 'KS ', 'KY ', 'LA ', 'ME', 'MD ', 'MA ', 'MI ', 
+			'MN ', 'NJ ', 'NM ', 'NY ', 'OH ', 'OK ', 'PA ', 'RI ','TN ', 'TX ', 
+			'UT ', 'VT ', 'VA ', 'WV ', 'WA ', 'WI ', 'WY ', 'DC ']
+
 }
