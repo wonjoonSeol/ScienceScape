2c84264dd9b592529b85881416cd62b34a572b82
Paul Muresan <k1631503@kcl.ac.uk>
2018-02-21 00:12:25 +0000

    Update comments (#16)

--- bibliotools3.0/scripts/utility.py
+++ bibliotools3.0/scripts/utility.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+#outer modules
 import os
 import sys
 import glob
@@ -9,7 +10,11 @@ import re
 
 #personal modules
 import utilities
-
+'''
+This class contains logic for various utilities used thoughout the scripts.
+It has a collection of all the items from the .dat files and the WOS Lines from 
+the initial files.
+'''
 class Utility:
     # TODO Use config file accessors instead when the config file is working!
     #list for all the utily objects
@@ -27,7 +32,7 @@ class Utility:
     #dictionary for all the objects
     collection = { 'articles': __articles , 'authors' : __authors,
             'countries' : __countries, 'institutions' : __institutions,
-            'keywords' : __keywords, 'labs' : __labs, 'refs' : __refs,
+            'keywords' : __keywords, 'labs' : __labs, 'references' : __refs,
             'subjects' : __subjects, 'woslines' : __woslines }
 
     '''
@@ -39,7 +44,7 @@ class Utility:
         return os.path.splitext(base)[0]
 
     '''
-        Reads from a given file which is targeted to a specific object.
+        Reads from a given .dat file which is targeted to a specific object.
         Based on what object is chosen, a new object of that type is created,
         for every line of the file. Every line gets passed to its constructor.
     '''
@@ -49,14 +54,12 @@ class Utility:
             for line in content:
                 #parse line based on object type
                 object_name = Utility.file_name(file_path)
-                re_line = re.split(', | ', line)
+                re_line = re.split('\t', line)
                 Utility.collection[object_name].append(
-                    Utility.__new_object(object_name, re_line))
-
+                    Utility.new_object(object_name, re_line))
 
     '''
-        Takes a list of WOS txt files as source and uses the parser to generate
-        .dat files for the items in the collection.
+        Takes a WOS txt file as source and creates a WOS Line in the collection.
     '''
     def init_wos(src):
         with open(src) as f:
@@ -76,7 +79,7 @@ class Utility:
         Takes a list of .dat files corrresponding to the items in the
         collection, and populates the items based on the data.
     '''
-    def init_utilites(source_list):
+    def init_utilities(files_list):
         #after we have .dat files created by parser, use utility to store them
         #in the collection
         for path in files_list:
@@ -101,7 +104,7 @@ class Utility:
         elif(object_name == 'institutions'):
             from utilites import institution
             return institution.Institution(line)
-        elif(object_name == 'keywords'):
+        elif('keywords' in object_name):
             from utilites import keyword
             return keyword.Keyword(line)
         elif(object_name == 'labs'):
@@ -116,3 +119,6 @@ class Utility:
         elif(object_name == 'woslines'):
             from utilities import wosline
             return wosline.WosLine(line)
+
+    def __str__():
+       return str(collection) 
