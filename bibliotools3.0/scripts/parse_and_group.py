0b2d950da6c2c7e6121b8dd6240fca8be7e475da
Nicolas Volken <32220848+nicolasvolken@users.noreply.github.com>
2018-02-24 10:45:52 +0000

    Refactor parse_and_group.py

--- bibliotools3.0/scripts/parse_and_group.py
+++ bibliotools3.0/scripts/parse_and_group.py
@@ -9,12 +9,6 @@ This script will separate the publications in the data files by year,
 and parse each data line to extract some information.
 '''
 
-'''
-File: parse_and_group.py
-This script will separate the publications in the data files by year,
-and parse each data line to extract some information.
-'''
-
 def is_year_within_span(startYear, endYear, year):
 	if year >= startYear and year <= endYear:
 		return True
@@ -55,36 +49,35 @@ def parse_span(span, input_dir, output_dir, outdir_prefix, files):
 	# Use Wos_parser function from parser.py to parse the lines
 	Wos_parser(os.path.join(input_dir, output_dir, span), os.path.join(outdir_prefix, span), True)
 
+
+def parse_and_group_data(one_file_corpus, output_dir, outdir_prefix, span_items):
+	input_dir = os.path.dirname(one_file_corpus)
+
+	# Collect the user-defined year span preferences
+	years_spans = dict((s, data["years"]) for s, data in span_items)
+	files = {}
+
+	if not os.path.exists(os.path.join(input_dir, output_dir)):
+		os.mkdir(os.path.join(input_dir, output_dir))
+	if not os.path.exists(outdir_prefix):
+		os.mkdir(outdir_prefix)
+
+	# Create one txt file for each user-defined year span
+	create_span_files(years_spans, input_dir, output_dir, files)
+
+	onefile_output = open(CONFIG["one_file_corpus"], "r")
+	onefile_output.readline()
+	lines_to_write = onefile_output.readlines()
+
+	# Write lines to the adequate span file
+	for line in lines_to_write:
+		separate_years(line, years_spans, files)
+
+	onefile_output.close()
+
+	for (span,ys) in years_spans.items():
+		parse_span(span, input_dir, output_dir, outdir_prefix, files)	# For each span, parse its data
+
+
 # -- Main Script --
-
-input_dir = os.path.dirname(CONFIG["one_file_corpus"])
-output_dir = CONFIG["wos_data_grouped"]
-outdir_prefix = CONFIG["parsed_data"]
-
-# Collect the user-defined year span preferences
-years_spans = dict((s, data["years"]) for s, data in CONFIG["spans"].items())
-
-files = {}	# Collection of span files
-
-if not os.path.exists(os.path.join(input_dir, output_dir)):
-	os.mkdir(os.path.join(input_dir, output_dir))
-
-if not os.path.exists(outdir_prefix):
-	os.mkdir(outdir_prefix)
-
-# Create one txt file for each user-defined year span
-create_span_files(years_spans, input_dir, output_dir, files)
-
-onefile_output = open(CONFIG["one_file_corpus"], "r")
-onefile_output.readline()
-
-lines_to_write = onefile_output.readlines()
-
-# Write lines to the adequate span file
-for line in lines_to_write:
-	separate_years(line, years_spans, files)
-
-onefile_output.close()
-
-for (span,ys) in years_spans.items():
-	parse_span(span, input_dir, output_dir, outdir_prefix, files)	# For each span, parse its data
+parse_and_group_data(CONFIG["one_file_corpus"], CONFIG["wos_data_grouped"], CONFIG["parsed_data"], CONFIG["spans"].items())
