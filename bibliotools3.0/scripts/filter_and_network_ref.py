df6ca982bc7375be53910cb4b304b4673c3437bd
Nicolas Volken <32220848+nicolasvolken@users.noreply.github.com>
2018-02-26 09:28:26 +0000

    Remove calls to CONFIG in group_by_article()

--- bibliotools3.0/scripts/filter_and_network_ref.py
+++ bibliotools3.0/scripts/filter_and_network_ref.py
@@ -7,30 +7,31 @@ import codecs
 import matplotlib.pyplot as plt
 from config import CONFIG
 
+
 def add_edge_weight(graph, node1, node2):
     if graph.has_edge(node1, node2):
         graph[node1][node2]['weight'] += 1
     else:
         graph.add_edge(node1, node2, weight = 1)
 
-def export(graph, span, export_ref_format):
+def export(graph, span, export_ref_format, parsed_data_folder):
     if export_ref_format == "gexf":
         print("Writing .gexf export")
-        networkx.write_gexf(graph, os.path.join(CONFIG["parsed_data"], span, "%s.gexf" %span), encoding = "UTF-8")
+        networkx.write_gexf(graph, os.path.join(parsed_data_folder, span, "%s.gexf" %span), encoding = "UTF-8")
     elif export_ref_format == "edgelist":
         print("Writing .csv export")
-        networkx.write_weighted_edgelist(graph, os.path.join(CONFIG["parsed_data"], span, "%s.csv"%span), delimiter = "\t")
+        networkx.write_weighted_edgelist(graph, os.path.join(parsed_data_folder, span, "%s.csv"%span), delimiter = "\t")
     elif export_ref_format == "pajek":
         print("Writing .pajek export")
-        networkx.write_pajek(graph, os.path.join(CONFIG["parsed_data"], span, "%s.net" %span), encoding = 'UTF-8')
+        networkx.write_pajek(graph, os.path.join(parsed_data_folder, span, "%s.net" %span), encoding = 'UTF-8')
     elif export_ref_format == "json":
         print("Writing .json export")
         data = json_graph.node_link_data(graph)
-        json.dump(data, open(os.path.join(CONFIG["parsed_data"], span, "%s.json" %span), "w"), encoding = 'UTF-8')
+        json.dump(data, open(os.path.join(parsed_data_folder, span, "%s.json" %span), "w"), encoding = 'UTF-8')
     else:
         print("No export compatible with the specified export format!")
 
-def group_by_article(references_by_articles_filtered, span, references_occs):
+def group_by_article(references_by_articles_filtered, span, references_occs, export_ref_format, parsed_data_folder):
     print("Processing edges for references...")
     graph = networkx.Graph()
     for article, art_refs in itertools.groupby(references_by_articles_filtered, key = lambda e:e[0]):
@@ -46,7 +47,7 @@ def group_by_article(references_by_articles_filtered, span, references_occs):
     networkx.set_node_attributes(graph, 'type', "reference")
 
     # Write export file
-    export(graph, span, CONFIG["export_ref_format"])
+    export(graph, span, export_ref_format, parsed_data_folder)
 
 # -- Main script --
 for span in sorted(CONFIG["spans"]):
@@ -80,4 +81,4 @@ for span in sorted(CONFIG["spans"]):
     references_by_articles_filtered.sort(key = lambda e:e[0])  # Sort References
 
     # Group by Article and Export
-    group_by_article(references_by_articles_filtered, span, references_occs)
+    group_by_article(references_by_articles_filtered, span, references_occs, CONFIG["export_ref_format"], CONFIG["parsed_data"])
