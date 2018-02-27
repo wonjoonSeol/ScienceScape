865218fa0cf2a1d02deb695e9ea334512061fabd
Nicolas Volken <nicolas.volken@kcl.ac.uk>
2018-02-27 22:25:08 +0000

    Refactor parser.py functions (#18)

--- mysite/bibliotools3.0/scripts/parser.py
+++ mysite/bibliotools3.0/scripts/parser.py
@@ -47,7 +47,74 @@ def parse_authors(id, article, output):
             name = first_name + ' ' + last_name
             output.write(f'{id}\t{i}\t{name}\n')
 
+def parse_keywords(id, article, f_article_keywords, f_isi_keywords, f_title_keywords, punctuation_storage, common_words_storage):
+    if(getattr(article, author_keywords) != ""):
+        article_author_keywords = getattr(article, author_keywords).split('; ')
+        for f in article_author_keywords:
+            f_article_keywords.write("%d\tAK\t%s\n" % (id,f.upper()))
+    if(getattr(article, keywords_plus) != ""):
+        article_author_keywords = getattr(article, keywords_plus).split('; ')
+        for f in article_author_keywords:
+            f_isi_keywords.write("%d\tIK\t%s\n" % (id,f.upper()))
+    if(getattr(article, document_title) != ""):
+        article_author_keywords = getattr(article, document_title)
+        #... remove punctuations !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~
+        for p in punctuation_storage: article_author_keywords = article_author_keywords.replace(p,'')
+        article_author_keywords = article_author_keywords.split(' ')
+        for f in article_author_keywords:
+            lowercase = f.lower()
+            if lowercase not in common_words_storage and len(lowercase)>0:
+                f_title_keywords.write("%d\tTK\t%s\n" % (id, lowercase.upper()))
 
+def parse_subjects(id, article, output):
+    if(getattr(article, wos_categories) != ""):
+        article_wos_cat = getattr(article, wos_categories).split('; ')
+        for i in range(len(article_wos_cat)):
+            output.write("%d\t%s\n" % (id,article_wos_cat[i]))
+
+
+def parse_references(id, article, collection_references, output):
+    computed_refs_local = 0
+    computed_corrupt_refs_local = 0
+    if(getattr(article, cited_references) != ""):
+         article_refs = getattr(article, cited_references).split('; ')
+         for i in range(len(article_refs)):
+            ref = utility.Utility.new_object('refs', article_refs[i])
+            collection_references.append(ref)
+            computed_refs_local += 1
+
+            if(ref.year > 0):
+                output.write("%d\t%s\t%d\t%s\t%s\t%s\n" %
+                         (id,ref.firstAU,ref.year,ref.journal,ref.volume,ref.page))
+            if(ref.year == 0): computed_corrupt_refs_local += 1
+    return (computed_refs_local, computed_corrupt_refs_local)
+
+def parse_countries_and_institutions(id, article, f_institutions, f_countries, usa_country_codes_storage):
+    if(getattr(article, author_address) != ""):
+        address = getattr(article, author_address)
+        aux1 = address.find('[')
+        aux2 = address.find(']')
+
+        while (aux1 < aux2):
+            aux = address[aux1:aux2+2]
+            address = address.replace(aux,'')
+            aux1 = address.find('[')
+            aux2 = address.find(']')
+
+        article_address = address.split('; ')
+        for i in range(len(article_address)):
+            article_address[i] = article_address[i].replace(', ', ',')
+            split_address = article_address[i].split(',')
+            length_of_address = len(split_address)
+
+            for j in range(length_of_address - 2):
+                f_institutions.write("%d\t%d\t%s\n" % (id,i,split_address[j]))
+
+            country = split_address[length_of_address-1]
+            length_split_address = len(split_address[length_of_address-1])
+
+            if  country[length_split_address-3 : length_split_address] == 'USA' or country[0:3] in usa_country_codes_storage:
+                    f_countries.write("%d\t%d\t%s\n" % (id,i,country))
 
 def Wos_parser(in_dir, out_dir, verbose):
 
@@ -71,8 +138,6 @@ def Wos_parser(in_dir, out_dir, verbose):
 
     WOS_IDS = dict()  # list the articles' wos-ids
     collection = utility.Utility.collection
-    # Treat Data
-
 
     for src in srclst:
         utility.Utility.init_wos(src)
@@ -92,71 +157,19 @@ def Wos_parser(in_dir, out_dir, verbose):
                 #authors
                 parse_authors(id, article, f_authors)
 
-
                 #keywords
-                if(getattr(article, author_keywords) != ""):
-                    article_author_keywords = getattr(article, author_keywords).split('; ')
-                    for f in article_author_keywords:
-                        f_article_keywords.write("%d\tAK\t%s\n" % (id,f.upper()))
-                if(getattr(article, keywords_plus) != ""):
-                    article_author_keywords = getattr(article, keywords_plus).split('; ')
-                    for f in article_author_keywords:
-                        f_isi_keywords.write("%d\tIK\t%s\n" % (id,f.upper()))
-                if(getattr(article, document_title) != ""):
-                    article_author_keywords = getattr(article, document_title)
-                    #... remove ponctuations !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~
-                    for p in CONFIG['punctuation']: article_author_keywords = article_author_keywords.replace(p,'')
-                    article_author_keywords = article_author_keywords.split(' ')
-                    for f in article_author_keywords:
-                        lowercase = f.lower()
-                        if lowercase not in CONFIG['common_words'] and len(lowercase)>0:
-                            f_title_keywords.write("%d\tTK\t%s\n" % (id, lowercase.upper()))
+                parse_keywords(id, article, f_article_keywords, f_isi_keywords, f_title_keywords, CONFIG['punctuation'], CONFIG['common_words'])
 
                 #subjects
-                if(getattr(article, wos_categories) != ""):
-                    article_wos_cat = getattr(article, wos_categories).split('; ')
-                    for i in range(len(article_wos_cat)):
-                        f_subjects.write("%d\t%s\n" % (id,article_wos_cat[i]))
+                parse_subjects(id, article, f_subjects)
 
                 #references
-                if(getattr(article, cited_references) != ""):
-                     article_refs = getattr(article, cited_references).split('; ')
-                     for i in range(len(article_refs)):
-                        ref = utility.Utility.new_object('refs', article_refs[i])
-                        collection['references'].append(ref)
-                        computed_refs += 1
-
-                        if(ref.year > 0):
-                            f_refs.write("%d\t%s\t%d\t%s\t%s\t%s\n" %
-                                     (id,ref.firstAU,ref.year,ref.journal,ref.volume,ref.page))
-                        if(ref.year == 0): computed_corrupt_refs += 1
+                parsed_references_stats = parse_references(id, article, collection["references"], f_refs)
+                computed_refs = parsed_references_stats[0]
+                computed_corrupt_refs = parsed_references_stats[1]
 
                 #countries / institutions
-                if(getattr(article, author_address) != ""):
-                    address = getattr(article, author_address)
-                    aux1 = address.find('[')
-                    aux2 = address.find(']')
-
-                    while (aux1 < aux2):
-                        aux = address[aux1:aux2+2]
-                        address = address.replace(aux,'')
-                        aux1 = address.find('[')
-                        aux2 = address.find(']')
-
-                    article_address = address.split('; ')
-                    for i in range(len(article_address)):
-                        article_address[i] = article_address[i].replace(', ', ',')
-                        split_address = article_address[i].split(',')
-                        length_of_address = len(split_address)
-
-                        for j in range(length_of_address - 2):
-                            f_institutions.write("%d\t%d\t%s\n" % (id,i,split_address[j]))
-
-                        country = split_address[length_of_address-1]
-                        length_split_address = len(split_address[length_of_address-1])
-
-                        if  country[length_split_address-3 : length_split_address] == 'USA' or country[0:3] in CONFIG['usa_country_codes']:
-                                f_countries.write("%d\t%d\t%s\n" % (id,i,country))
+                parse_countries_and_institutions(id, article, f_institutions, f_countries, CONFIG["usa_country_codes"])
 
     # End
     if verbose: print(("..%d parsed articles in total") % (id + 1))
