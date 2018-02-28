22cc8f7f336dccb9f00f92e824df13050d52f816
Paul Muresan <k1631503@kcl.ac.uk>
2018-02-28 08:50:52 +0000

    Optimise parser.py (#18)

--- mysite/bibliotools3.0/scripts/parser.py
+++ mysite/bibliotools3.0/scripts/parser.py
@@ -3,9 +3,11 @@ import sys
 import glob
 import argparse
 import utility
+import re
+import string
 from config import CONFIG
 
-# TODO Link with config file
+# Header keywords
 accession_number = CONFIG['accession_number']
 authors = CONFIG['authors']
 author_keywords = CONFIG['author_keywords']
@@ -24,7 +26,6 @@ document_type = CONFIG['document_type']
 wos_core_collection_times_cited = CONFIG['wos_core_collection_times_cited']
 
 def parse_article(id, article, output):
-    print(str(id) + 'im here')
     article_authors = getattr(article, authors).split('; ')
     firstAU = article_authors[0].replace(',','')
     output.write("%d\t%s\t%d\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" %
@@ -35,58 +36,62 @@ def parse_article(id, article, output):
         getattr(article, document_type), getattr(article, wos_core_collection_times_cited),
         getattr(article, document_title), getattr(article, accession_number)))
 
-
 def parse_authors(id, article, output):
     if getattr(article, authors) != '':
         article_authors = getattr(article, authors).split('; ')
-
-        for i in range(len(article_authors)):
-            article_authors[i] = article_authors[i].replace(',','')
-            first_name = article_authors[i].split(' ')[0].capitalize()
-            last_name = article_authors[i].split(' ')[1].capitalize()
+        for author in article_authors:
+            position = article_authors.index(author)
+            author = author.replace(',', '')
+            first_name = author.split(' ')[0].capitalize()
+            last_name = author.split(' ')[1].capitalize()
             name = first_name + ' ' + last_name
-            output.write(f'{id}\t{i}\t{name}\n')
+            output.write(f'{id}\t{position}\t{name}\n')
 
-def parse_keywords(id, article, f_article_keywords, f_isi_keywords, f_title_keywords, punctuation_storage, common_words_storage):
-    if(getattr(article, author_keywords) != ""):
+def parse_author_keywords(id, article, output):
+    if getattr(article, author_keywords) != '':
         article_author_keywords = getattr(article, author_keywords).split('; ')
-        for f in article_author_keywords:
-            f_article_keywords.write("%d\tAK\t%s\n" % (id,f.upper()))
-    if(getattr(article, keywords_plus) != ""):
-        article_author_keywords = getattr(article, keywords_plus).split('; ')
-        for f in article_author_keywords:
-            f_isi_keywords.write("%d\tIK\t%s\n" % (id,f.upper()))
-    if(getattr(article, document_title) != ""):
-        article_author_keywords = getattr(article, document_title)
-        #... remove punctuations !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~
-        for p in punctuation_storage: article_author_keywords = article_author_keywords.replace(p,'')
-        article_author_keywords = article_author_keywords.split(' ')
-        for f in article_author_keywords:
-            lowercase = f.lower()
-            if lowercase not in common_words_storage and len(lowercase)>0:
-                f_title_keywords.write("%d\tTK\t%s\n" % (id, lowercase.upper()))
+        for keyword in article_author_keywords:
+            output_keyword = keyword.upper()
+            output.write(f'{id}\tAK\t{output_keyword}\n')
+
+def parse_isi_keywords(id, article, output):
+    if getattr(article, keywords_plus) != '':
+        isi_keywords = getattr(article, keywords_plus).split('; ')
+        for keyword in isi_keywords:
+            output_keyword = keyword.upper()
+            output.write(f'{id}\tIK\t{output_keyword}\n')
+
+def parse_title_keywords(id, article, output):
+    if getattr(article, document_title) != '':
+        title_keywords = getattr(article, document_title)
+        #remove punctuation
+        regex = re.compile('[%s]' % re.escape(string.punctuation))
+        title_keywords = regex.sub('', title_keywords)
+        for keyword in title_keywords.split(' '):
+            output_keyword = keyword.upper()
+            if keyword.lower() not in CONFIG['common_words'] and keyword != '':
+                output.write(f'{id}\tTK\t{output_keyword}\n')
 
 def parse_subjects(id, article, output):
-    if(getattr(article, wos_categories) != ""):
-        article_wos_cat = getattr(article, wos_categories).split('; ')
-        for i in range(len(article_wos_cat)):
-            output.write("%d\t%s\n" % (id,article_wos_cat[i]))
-
+    if getattr(article, wos_categories) != '':
+        article_subjects = getattr(article, wos_categories).split('; ')
+        for subject in article_subjects:
+            output.write(f'{id}\t{subject}\n')
 
 def parse_references(id, article, collection_references, output):
     computed_refs_local = 0
     computed_corrupt_refs_local = 0
-    if(getattr(article, cited_references) != ""):
+    if getattr(article, cited_references) != '':
          article_refs = getattr(article, cited_references).split('; ')
          for i in range(len(article_refs)):
             ref = utility.Utility.new_object('refs', article_refs[i])
-            collection_references.append(ref)
             computed_refs_local += 1
 
             if(ref.year > 0):
-                output.write("%d\t%s\t%d\t%s\t%s\t%s\n" %
-                         (id,ref.firstAU,ref.year,ref.journal,ref.volume,ref.page))
-            if(ref.year == 0): computed_corrupt_refs_local += 1
+                output.write(f"{id}\t{ref.firstAU}\t{ref.year}\t{ref.journal}\
+                \t{ref.volume}\t{ref.page}\n")
+            elif(ref.year == 0):
+                computed_corrupt_refs_local += 1
     return (computed_refs_local, computed_corrupt_refs_local)
 
 def parse_countries_and_institutions(id, article, f_institutions, f_countries, usa_country_codes_storage):
@@ -153,12 +158,12 @@ def Wos_parser(in_dir, out_dir, verbose):
 
                 #article
                 parse_article(id, article, f_articles)
-
                 #authors
                 parse_authors(id, article, f_authors)
-
                 #keywords
-                parse_keywords(id, article, f_article_keywords, f_isi_keywords, f_title_keywords, CONFIG['punctuation'], CONFIG['common_words'])
+                parse_author_keywords(id, article, f_article_keywords)
+                parse_isi_keywords(id, article, f_isi_keywords)
+                parse_title_keywords(id, article, f_title_keywords)
 
                 #subjects
                 parse_subjects(id, article, f_subjects)
