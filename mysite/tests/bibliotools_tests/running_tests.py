1726be04ed38648ae5d65ef1eae9935c5922f379
Britton Forsyth <britton.forsyth@kcl.ac.uk>
2018-03-08 14:17:53 +0000

    Add script to run testing suite

---
+++ mysite/tests/bibliotools_tests/running_tests.py
@@ -0,0 +1,24 @@
+"""
+This is a testing file to run the test suite for Bibliotools 3.
+"""
+
+print("Running CONFIG test suite...")
+import config_test
+
+print("Running CORPUS_PARSED_OVERVIEW test suite...")
+import corpus_parsed_overview_test
+
+print("Running FILTER_AND_NETWORK_REF test suite...")
+import filter_and_network_ref_test
+
+print("Running MERGING_CORPUS test suite...")
+import merging_corpus_test
+
+print("Running PARSE_AND_GROUP test suite...")
+import parse_and_group_test
+
+print("Running PARSERS_TEST test suite...")
+import parsers_test
+
+print(">>> Done. All tests executed successfully.")
+
