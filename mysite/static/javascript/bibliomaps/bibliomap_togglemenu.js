45f93a8c8ede954ccaabc2b4c9a9d8805dfd3f1d
Wonjoon Seol <wonjoon.seol@kcl.ac.uk>
2018-03-14 18:38:06 +0000

    Move mysite contents to the root

--- mysite/static/javascript/bibliomaps/bibliomap_togglemenu.js
+++
@@ -1,9 +0,0 @@
-function togglemenu(mytoggleID){
-  //show right submenu
-  d3.select("#toggleID1").attr( 'class', 'hidden_submenu')
-  d3.select("#toggleID2").attr( 'class', 'hidden_submenu')
-  d3.select("#toggleID3").attr( 'class', 'hidden_submenu')
-  d3.select("#toggleID4").attr( 'class', 'hidden_submenu')
-  d3.select("#toggleID"+mytoggleID).attr( 'class', 'active_submenu')
-
-}
