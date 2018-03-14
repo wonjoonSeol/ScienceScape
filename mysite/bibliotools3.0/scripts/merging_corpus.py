1ffbb65734cdd920fd7607943f212ec65c408074
Britton Forsyth <bforsyth927@gmail.com>
2018-03-14 13:25:10 +0000

    Document public methods of merging_corpus

--- mysite/bibliotools3.0/scripts/merging_corpus.py
+++ mysite/bibliotools3.0/scripts/merging_corpus.py
@@ -10,6 +10,9 @@ all parseable lines to a single file, Result/one_file_corpus.txt
 
 CONFIG = {}
 
+"""
+Writes year distributions for all spans into a CSV file.
+"""
 def write_year_distribution(reports_directory, years_spans):
     years_distribution = open(os.path.join(reports_directory, "years_distribution.csv"), "w")
     years_distribution.write("year,nb_articles\n")
@@ -21,6 +24,9 @@ def write_year_distribution(reports_directory, years_spans):
     years_distribution.close()
     print("\nYear distribution reported in %s" %os.path.join(reports_directory,"years_distribution.csv"))
 
+"""
+Counts occurrences of years within years_spans and reports the resulting year distribution.
+"""
 def count_occurences(one_file_corpus, reports_directory):
     # Output the article numbers by year
     years_spans = {}
@@ -36,14 +42,20 @@ def count_occurences(one_file_corpus, reports_directory):
     onefile_output.close()
     write_year_distribution(reports_directory, years_spans)     # Report year distribution for information
 
-
+"""
+Writes text to a file.
+"""
 def write_to_file(open_file, text):
     open_file.write(text)
 
+""" Return the number of columns present in a line of text, split by tab. """
 def number_columns(line):
     return len(line.split("\t"))
 
-
+""" Return an integer that is set to 1 if the line was stripped of a trailing tab.
+Parse an input line, filtering out blank lines and ensuring that the number of columns
+corresponds to the selected headers.
+"""
 def parse_line(l, nb_values_in_wos, parseable_lines, lines_with_errors):
     repaired = 0
     if "\t" in l:
@@ -62,11 +74,17 @@ def parse_line(l, nb_values_in_wos, parseable_lines, lines_with_errors):
             parseable_lines.append(l)
     return repaired     # For statistics
 
+"""
+Write the parsed output and errors output to result files.
+"""
 def write_report(parseable_lines, lines_with_errors, onefile_output, errorsfile_output):
     write_to_file(onefile_output, "\n".join(parseable_lines) + "\n")
     write_to_file(errorsfile_output, "\n".join(lines_with_errors) + "\n")
     print("Found %s non-parseable lines, reported in wos_lines_with_errors.csv" %(len(lines_with_errors)))
 
+""" Return a counter of all lines that were repaired in the file.
+Parse an entire file, removing the headers and parsing each line individually.
+"""
 def parse_file(file, root, nb_values_in_wos, onefile_output, errorsfile_output):
     new_trailing_tabs = 0
     if not file.startswith('.'):
@@ -87,13 +105,18 @@ def parse_file(file, root, nb_values_in_wos, onefile_output, errorsfile_output):
             write_report(parseable_lines, lines_with_errors, onefile_output, errorsfile_output)
     return new_trailing_tabs
 
+""" Return the final output file.
+Construct output file.
+"""
 def prepare_output_file(one_file_corpus, wos_headers):
     if not os.path.exists(os.path.dirname(one_file_corpus)):
         os.makedirs(os.path.dirname(one_file_corpus))
     onefile_output = open(one_file_corpus, "w")
     write_to_file(onefile_output, wos_headers + "\n")
     return onefile_output
-
+"""
+Construct a directory to place reports in.
+"""
 def prepare_report_directory(reports_directory):
     if not os.path.exists(reports_directory):
         os.mkdir(reports_directory)
@@ -101,11 +124,17 @@ def prepare_report_directory(reports_directory):
         print("Remove file %s or change 'reports_directory' value in config.py" %reports_directory)
         exit()
 
+"""
+Construct a file to place error reports in.
+"""
 def prepare_error_file(reports_directory, wos_headers):
     errorsfile_output = open(os.path.join(reports_directory, "wos_lines_with_errors.csv"), "w")
     write_to_file(errorsfile_output, wos_headers + "\n")
     return errorsfile_output
 
+"""
+Parse and merge all output files in the WOS corpus into one.
+"""
 def merge_corpus(one_file_corpus, wos_headers, reports_directory, wos_data):
     nb_values_in_wos = len(wos_headers.split("\t"))
 
