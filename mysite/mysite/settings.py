925cf474c4dfb2a968b195ec1ad69370875b3045
Wonjoon Seol <wonjoon.seol@kcl.ac.uk>
2018-03-14 18:13:09 +0000

    Update Database url to match heroku

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,17 @@ MEDIA_URL = '/media/'
 
 #DATABASE_URL = os.environ['DATABASE_URL']
 DATABASE_URL = 'postgres://fymhuaixuttaky:bd01de5c5ba9a57c40bb3ee84793c7ba649b0bacb6d5d8b4d8c23559d6994495@ec2-54-197-254-189.compute-1.amazonaws.com:5432/d11cvaqi49gegm'
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd11cvaqi49gegm',
+        'USER': 'fymhuaixuttaky',
+        'PASSWORD': 'bd01de5c5ba9a57c40bb3ee84793c7ba649b0bacb6d5d8b4d8c23559d6994495',
+        'HOST': 'ec2-54-197-254-189.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
