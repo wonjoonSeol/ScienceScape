3af448e0b5b40fa1a94a1efbd3c19b102a740789
Wonjoon Seol <wonjoon.seol@kcl.ac.uk>
2018-03-15 12:10:06 +0000

    Add static dir to settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -111,6 +111,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static')]
 STATIC_USER_FILES_DIRECTORY = "static/userFiles"
 PUBLIC_USER_FILES_DIRECTORY = "static/userFiles/Public"
 
