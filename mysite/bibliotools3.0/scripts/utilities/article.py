0703ec83a55176b4e1c84ae3e87c3a31c39514af
Paul Muresan <mmuresanpaul@gmail.com>
2018-03-15 13:45:13 +0000

    Comment article.py (#79)

--- mysite/bibliotools3.0/scripts/utilities/article.py
+++ mysite/bibliotools3.0/scripts/utilities/article.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env python
 
-
+"""
+    A class that holds parsed data from an Article.
+"""
 class Article:
 
     def __init__(self, data):
         self.id          = 0
-        self.firstAU     = ""       
+        self.firstAU     = ""
         self.year        = 0
         self.journal     = ""
         self.volume      = ""
@@ -18,17 +20,28 @@ class Article:
         self.uniqueID    = ""
 
         self.id = int(data[0])
-        if(len(data)>1): self.firstAU = data[1]
-        if(len(data)>2): self.year = int(data[2]) 
-        if(len(data)>3): self.journal = data[3] 
-        if(len(data)>4): self.volume = data[4] 
-        if(len(data)>5): self.page = data[5] 
-        if(len(data)>6): self.doi  = data[6]
-        if(len(data)>7): self.pubtype = data[7]
-        if(len(data)>8): self.doctype = data[8]
-        if(len(data)>9): self.times_cited = data[9]
-        if(len(data)>10): self.title = data[10]
-        if(len(data)>11): self.uniqueID = data[11]
+        if len(data) > 1:
+            self.firstAU = data[1]
+        if len(data) > 2:
+            self.year = int(data[2])
+        if len(data) > 3:
+            self.journal = data[3]
+        if len(data) > 4:
+            self.volume = data[4]
+        if len(data) > 5:
+            self.page = data[5]
+        if len(data) > 6:
+            self.doi  = data[6]
+        if len(data) > 7:
+            self.pubtype = data[7]
+        if len(data) > 8:
+            self.doctype = data[8]
+        if len(data) > 9:
+            self.times_cited = data[9]
+        if len(data) > 10:
+            self.title = data[10]
+        if len(data) > 11:
+            self.uniqueID = data[11]
 
     def __str__(self):
         return 'article no ' + id + ': ' + title + '\n'
