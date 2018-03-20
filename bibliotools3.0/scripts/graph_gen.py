fbebd1b2575ef89e87e4678e987423f99805085c
Paul Muresan <mmuresanpaul@gmail.com>
2018-03-20 18:07:16 +0000

    Scripts work both as standalone and with website users

--- bibliotools3.0/scripts/graph_gen.py
+++ bibliotools3.0/scripts/graph_gen.py
@@ -3,23 +3,23 @@ import argparse
 # Prompt the user for a time span.
 parser = argparse.ArgumentParser(description='For each time span enter a range \"YYYY-YYYY\"')
 parser.add_argument('-bound', nargs='+', metavar='bound', help='Year value', default={"1900-1999", "2000-2018"})
-parser.add_argument('-user')
+parser.add_argument('-user', default=' ')
 parser.add_argument('-headers', default='UT AU DE ID TI WC CR C1 PY J9 VL BP DI PT DT TC',\
-                        help='Accession Number\n\
-                                Authors\n\
-                                Author Keywords\n\
-                                Keywords Plus\n\
-                                Document Title\n\
-                                Wos Categories\n\
-                                Cited References\n\
-                                Author Address\n\
-                                Year Published\n\
-                                Twenty Nine Character Source Abbreviation\n\
-                                Volume\n\
-                                Beginning Page\n\
-                                Doi\n\
-                                Publication Type\n\
-                                Document Type\n\
+                        help='Accession Number, \n\
+                                Authors, \n\
+                                Author Keywords, \n\
+                                Keywords Plus, \n\
+                                Document Title, \n\
+                                Wos Categories, \n\
+                                Cited References, \n\
+                                Author Address, \n\
+                                Year Published, \n\
+                                Twenty Nine Character Source Abbreviation, \n\
+                                Volume, \n\
+                                Beginning Page, \n\
+                                Doi, \n\
+                                Publication Type, \n\
+                                Document Type, \n\
                                 Wos Core Collection Times Cited')
 args = parser.parse_args()
 
