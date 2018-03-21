ac737dcb6296db113fb09b6950c90ba3ac19b961
Paul Muresan <mmuresanpaul@gmail.com>
2018-03-21 17:14:47 +0000

    Fix header arguments & add commments+helpers.

--- bibliotools3.0/scripts/graph_gen.py
+++ bibliotools3.0/scripts/graph_gen.py
@@ -1,26 +1,44 @@
 import argparse
 
-# Prompt the user for a time span.
+'''
+Prompt for time spans. This is used to split the data into several time spans.
+'''
 parser = argparse.ArgumentParser(description='For each time span enter a range \"YYYY-YYYY\"')
-parser.add_argument('-bound', nargs='+', metavar='bound', help='Year value', default={"1900-1999", "2000-2018"})
-parser.add_argument('-user', default=' ')
-parser.add_argument('-headers', default='UT AU DE ID TI WC CR C1 PY J9 VL BP DI PT DT TC',\
-                        help='Accession Number, \n\
-                                Authors, \n\
-                                Author Keywords, \n\
-                                Keywords Plus, \n\
-                                Document Title, \n\
-                                Wos Categories, \n\
-                                Cited References, \n\
-                                Author Address, \n\
-                                Year Published, \n\
-                                Twenty Nine Character Source Abbreviation, \n\
-                                Volume, \n\
-                                Beginning Page, \n\
-                                Doi, \n\
-                                Publication Type, \n\
-                                Document Type, \n\
-                                Wos Core Collection Times Cited')
+parser.add_argument('-bound', nargs='+', metavar='bound', help='Year value (xxxx-xxxx, ...)', default={"1900-1999", "2000-2018"})
+
+'''
+Prompt for user name. This is for a web server setup.
+The stored location is the ScienceScape/static/userFiles. /
+ (If no user is added, then the bibliotools folder is used, without any user
+hierarchy.)
+'''
+parser.add_argument('-user', default=' ', help='Prompt the user for user name. \
+                                                This is for a web server setup.\
+                    The stored location is the ScienceScape/static/userFiles/. \
+(If no user is added, then the bibliotools folder is used, without any user \
+hierarchy.)')
+
+'''
+'''
+parser.add_argument('-headers', default='UT-AU-DE-ID-TI-WC-CR-C1-PY-J9-VL-BP-DI-PT-DT-TC',\
+                        help='Add the headers separated by dashes in the following\
+                                order: Accession Number, \
+                                Authors, \
+                                Author Keywords, \
+                                Keywords Plus, \
+                                Document Title, \
+                                Wos Categories, \
+                                Cited References, \
+                                Author Address, \
+                                Year Published, \
+                                Twenty Nine Character Source Abbreviation, \
+                                Volume, \
+                                Beginning Page, \
+                                Doi, \
+                                Publication Type, \
+                                Document Type, \
+                                Wos Core Collection Times Cited.  \
+                                Default is: UT-AU-DE-ID-TI-WC-CR-C1-PY-J9-VL-BP-DI-PT-DT-TC')
 args = parser.parse_args()
 
 spanYears = []
@@ -37,7 +55,7 @@ print(user)
 # Run all scripts.
 import config
 config.spanYears = spanYears
-CONFIG = config.gen(user, str(args.headers).split(' '))
+CONFIG = config.gen(user, str(args.headers).split('-'))
 
 print("\nMERGING CORPUS\n")
 import merging_corpus
