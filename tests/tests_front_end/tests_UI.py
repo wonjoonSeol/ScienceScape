a25dee3083ed282a21ed2d93ad4e9ffced15b923
Nicolas Volken <nicolas.volken@kcl.ac.uk>
2018-03-23 13:21:53 +0000

    Set up Django automated testing command (#215)

--- tests/tests_front_end/tests_UI.py
+++ tests/tests_front_end/tests_UI.py
@@ -4,8 +4,10 @@ from django.test import TestCase
 
 class TestUI(TestCase):
 
-    def test_homepage(self):
+    def test_homepage_shows_correct_information(self):
         self.browser_driver = webdriver.Chrome()
         self.browser_driver.get("http://127.0.0.1:8000/")
         self.assertIn("ScienceScape", self.browser_driver.page_source)
+        self.assertIn("ScienceScapeS - Scientometrics, made easy", self.browser_driver.title)
+        self.assertIn("Example Graph", self.browser_driver.page_source)
         self.browser_driver.quit()
