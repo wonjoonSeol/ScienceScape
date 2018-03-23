1b70c5d30acc865cc18ee9f01d3cb0eccdef5b93
Nicolas Volken <nicolas.volken@kcl.ac.uk>
2018-03-23 17:09:57 +0000

    Add selenium tests (#215)

---
+++ tests/tests_front_end/tests_homepage_information.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from django.test import TestCase
+
+class TestHomepageInformation(TestCase):
+
+    def test_homepage_shows_correct_information(self):
+        browser_driver = webdriver.Chrome()
+        browser_driver.get("http://127.0.0.1:8000/")
+        self.assertIn("ScienceScape", browser_driver.page_source)
+        self.assertIn("ScienceScapeS - Scientometrics, made easy", browser_driver.title)
+        self.assertIn("Example Graph", browser_driver.page_source)
+        browser_driver.quit()
+
+    def test_user_sees_login_button(self):
+        browser_driver2 = webdriver.Chrome()
+        browser_driver2.get("http://127.0.0.1:8000/")
+        page_source = browser_driver2.page_source
+        self.assertIn("Log In", page_source)
+        browser_driver2.quit()
