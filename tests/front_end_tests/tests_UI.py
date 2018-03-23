754fa83c87e20b93fd1bcd825065bf4a6f78b082
Nicolas Volken <nicolas.volken@kcl.ac.uk>
2018-03-23 13:12:28 +0000

    Simplify and fix Selenium testing file (#215)

--- tests/front_end_tests/tests_UI.py
+++ tests/front_end_tests/tests_UI.py
@@ -1,30 +1,11 @@
-"""import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from django.test import TestCase
 
-class Sciencescape(unittest.TestCase):
+class TestUI(TestCase):
 
-    def setUp(self):
-        ## Uses Chrome webdriver
-        self.driver = webdriver.Chrome()
-        ## Uses Firefox webdriver
-        #self.driver = webdriver.Firefox()
-
-    def test_title_in_sciencescape(self):
-        driver = self.driver
-        ## Localhost - link
-        #driver.get("http://127.0.0.1:8000/")
-        ## Heroku - link
-        driver.get("https://sciencescape.herokuapp.com/")
-        self.assertIn("ScienceScape", driver.title)
-        assert "No results found." not in driver.page_source
-
-    def tearDown(self):
-        ## Quits the browser entirely
-        self.driver.quit()
-        ## Closes the tab of the browser instead of quitting the browser entirely
-        #self.driver.close()
-
-if __name__ == "__main__":
-    unittest.main()
-"""
+    def test_homepage(self):
+        self.browser_driver = webdriver.Chrome()
+        self.browser_driver.get("http://127.0.0.1:8000/")
+        self.assertIn("ScienceScape", self.browser_driver.page_source)
+        self.browser_driver.quit()
